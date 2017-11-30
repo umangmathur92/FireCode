88fa885ea9fb618c1157e53b23284cda9360c492
umangmathur92 <umangmathur10051992@gmail.com>
2017-11-29 23:45:55 -0800

    .

--- src/level2/IterativePreOrderTraversalOfBinaryTree.java
+++ src/level2/IterativePreOrderTraversalOfBinaryTree.java
@@ -5,10 +5,17 @@ import java.util.ArrayList;
 public class IterativePreOrderTraversalOfBinaryTree {
 
     public static void main(String[] args) {
-
+        TreeNode treeNode = new TreeNode(5);
+        treeNode.left = new TreeNode(4);
+        treeNode.right = new TreeNode(6);
+        treeNode.left.left = new TreeNode(2);
+        treeNode.left.right = new TreeNode(3);
+        treeNode.right.left = new TreeNode(7);
+        treeNode.right.right = new TreeNode(9);
+        ArrayList<Integer> preorderItr = preorderItr(treeNode);
     }
 
-    public ArrayList<Integer> preorderItr(TreeNode root) {
+    public static ArrayList<Integer> preorderItr(TreeNode root) {
 
         return null;
     }
